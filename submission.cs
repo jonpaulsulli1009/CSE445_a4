2e1f73cb079e5017d87562b3411bb3e8df2abce5
jonpaulsulli1009 <jon.paul.sullivan.3143@gmail.com>
2025-11-05 00:00:06 -0700

    Update submission.cs

--- submission.cs
+++ submission.cs
@@ -33,6 +33,7 @@ namespace ConsoleApp1
 
         public static void Main(string[] args)
         {
+
             string result = Verification(xmlURL, xsdURL);
             Console.WriteLine(result);
 
@@ -55,10 +56,11 @@ namespace ConsoleApp1
             errorCount = 0;
             errorString.Clear();
 
-            try{
+            try
+            {
                 //add the xsd to our schema set
                 xss.Add(null, xsdUrl);
-            
+
                 //fix the reader settings and give it the schema
                 xrs.ValidationType = ValidationType.Schema;
                 xrs.Schemas = xss;
@@ -67,24 +69,35 @@ namespace ConsoleApp1
                 xrs.ValidationEventHandler += new ValidationEventHandler(MyValidationCallBack);
 
                 //make a reader and read the xml
-                XmlReader xr = XmlReader.Create(xmlUrl,xrs);
-                while(xr.Read());
+                XmlReader xr = XmlReader.Create(xmlUrl, xrs);
+
+                while (xr.Read()) ;
+
             }
-            
-            Console.WriteLine("validation complete");
-            
-            if(errorFree){
+            catch
+            {
+                
+            }
+
+
+
+            if (errorFree)
+            {
                 return "No Error";
-            }else{
+            }
+            else
+            {
                 return errorString.ToString();
             }
         }
 
-        private static void MyValidationCallBack(object sender, ValidationEventArgs e){
+        private static void MyValidationCallBack(object sender, ValidationEventArgs e)
+        {
             errorFree = false;
             errorCount++;
-            errorString.Append("Error {errorCount}: {e.Message}\n");
-        } 
+            errorString.Append($"Error {errorCount}: {e.Message}\n");
+
+        }
 
         public static string Xml2Json(string xmlUrl)
         {
@@ -93,63 +106,66 @@ namespace ConsoleApp1
             myHotels = new List<Hotel>();
             Hotel curHotel = null;
             Address curAddr = null;
-            List<String> curPhones = null;
+            List<string> curPhones = null;
             //start reading
             while(xr.Read()){
-                if(xr.NodeType == XmlNodeType.Element && xr.Name == "Hotel"){
-                    curHotel = new Hotel();
-                    curPhones = new List<string>();
-                    curAddr = new Address();
-                    curHotel.rate = xr.GetAttribute("Rating");
-                    XmlReader hotelSub = xr.ReadSubtree();
-                    while(hotelSub.Read()){
-                        switch(hotelSub.Name){
-                            case "Name":
-                                curHotel.name = hotelSub.ReadElementContentAsString();
-                                break;
-                            case "Phone":
-                                curHotel.phones.Add(hotelSub.ReadElementContentAsString());
-                                break;
-                            case "Address":
-                                curAddr.nearair = hotelSub.GetAttribute("NearestAirport");
-                                using(xmlReader addrSub = hotelSub.ReadSubtree()){
-                                    while(addrSub.Read()){
-                                        switch(addrSub.Name){
-                                            case "Number":
-                                                curAddr.snumber = addrSub.ReadElementContentAsString();
-                                                break;
-                                            case "Street":
-                                                curAddr.snumber = addrSub.ReadElementContentAsString();
-                                                break;
-                                            case "City":
-                                                curAddr.snumber = addrSub.ReadElementContentAsString();
-                                                break;
-                                            case "State":
-                                                curAddr.snumber = addrSub.ReadElementContentAsString();
-                                                break;
-                                            case "Zip":
-                                                curAddr.snumber = addrSub.ReadElementContentAsString();
-                                                break;
-                                        }
-                                    }
-                                }
-                                break;
-                        }
-                    }     
-                } else if (xr.NodeType == XmlNodeType.EndElement && xr.Name == "Hotel"){
+                if (xr.NodeType == XmlNodeType.Element)
+                {
+                    switch (xr.Name)
+                    {
+                        case "Hotel":
+                            curHotel = new Hotel();
+                            curPhones = new List<string>();
+                            curAddr = new Address();
+                            curHotel.rate = xr.GetAttribute("Rating");
+                            break;
+                        case "Name":
+                            curHotel.name = xr.ReadElementContentAsString();
+                            break;
+                        case "Phone":
+                            curPhones.Add(xr.ReadElementContentAsString());
+                            break;
+                        case "Address":
+                            curAddr.nearair = xr.GetAttribute("NearestAirport");
+                            break;
+                        case "Number":
+                            curAddr.snumber = xr.ReadElementContentAsString();
+                            break;
+                        case "Street":
+                            curAddr.street = xr.ReadElementContentAsString();
+                            break;
+                        case "City":
+                            curAddr.city = xr.ReadElementContentAsString();
+                            break;
+                        case "State":
+                            curAddr.state = xr.ReadElementContentAsString();
+                            break;
+                        case "Zip":
+                            curAddr.zip = xr.ReadElementContentAsString();
+                            break;
+                    }
+                }
+                else if (xr.NodeType == XmlNodeType.EndElement && xr.Name == "Hotel")
+                {
+                    // End of a hotel element, add to the list
                     curHotel.phones = curPhones;
                     curHotel.addr = curAddr;
-                    myHotles.Add(curHotel);
-                {
+                    myHotels.Add(curHotel);
+                }
+
             }
             Hotels allHotels = new Hotels();
             allHotels.Hotel = myHotels;
+
             JsonSerializerOptions options = new JsonSerializerOptions{WriteIndented = true, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull};
+
             string jsonText = JsonSerializer.Serialize(allHotels, options);
+
             // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
             return jsonText;
 
         }
+  
         private class Hotels{
             public List<Hotel> Hotel;
         }
@@ -170,6 +186,7 @@ namespace ConsoleApp1
     }
 
 }
+
 
 
 
